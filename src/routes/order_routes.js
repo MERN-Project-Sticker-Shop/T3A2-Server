6f2e67827f12ca97c26809fdd30d69a15fb8577b
Chengqunniu <chengqunniu@gmail.com>
2023-01-26 22:05:54 +1100

    Updated order routes to populate address info

--- src/routes/order_routes.js
+++ src/routes/order_routes.js
@@ -5,13 +5,13 @@ const router = express.Router()
 
 // Get all orders
 router.get('', async (req, res) => {
-  res.send(await OrderModel.find().populate('cart'))
+  res.send(await OrderModel.find().populate('cart').populate('address'))
 })
 
 // Get a single order
 router.get('/:orderid', async (req, res) => {
   try {
-    const order = await OrderModel.findOne({_id: req.params.orderid}).populate('cart')
+    const order = await OrderModel.findOne({_id: req.params.orderid}).populate('cart').populate('address')
     if (order) {
       res.send(order)
     } else {
