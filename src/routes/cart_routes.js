11c5b5126b237d949d6e6f9202f7ac091eb5cec5
Chengqunniu <chengqunniu@gmail.com>
2023-01-26 13:22:46 +1100

    Created functions to verify products and update quantity

--- src/routes/cart_routes.js
+++ src/routes/cart_routes.js
@@ -3,53 +3,80 @@ import { CartModel, ProductModel } from '../db.js'
 
 const router = express.Router()
 
+async function checkProduct(productName, quantity) {
+  const productObject = await ProductModel.findOne({ name: productName })
+  if (productObject) {
+    const newCartitem = {product: productObject.name, price: productObject.price, quantity}
+    return newCartitem
+  } else {
+    return 'Product not found!'
+  }
+}
+
+async function updateQuantity(id, name, result, res) {
+  //Find the existing cart model and obtain the cart items
+  const cartObject = await CartModel.findOne({ _id: id  })
+  const cartItem = cartObject.item
+  //Add new item to the cart items array
+  //If item already exists, update the quantity number
+  for (var i in cartItem) {
+    if (cartItem[i].product === name) {
+      cartItem[i].quantity += result.quantity
+    }}
+  //Updated cart item
+  const updatedCartitem = {item: cartItem}
+  try {
+    const newItem = await CartModel.findByIdAndUpdate(id, updatedCartitem, { new: true})
+    res.send(newItem)
+  }
+  catch(err) {
+    res.status(500).send({ error: err.message })
+  }
+}
+
 //Get all cart items
 router.get('', async (req, res) => {
   res.send(await CartModel.find())
 })
 
 //Add products to cart
-router.post('/:name', async (req, res) => {
-  try {
-    const productName = req.params.name
-    //Find the product
-    const productObject = await ProductModel.findOne({ name: productName })
-    const { quantity } = req.body
-    const newCartitem = { item: [{product: productObject.name, price: productObject.price, quantity}] }
-    const insertedCartitem = await CartModel.create(newCartitem)
-    res.status(201).send(insertedCartitem)
-    }
-  catch(err) {
+router.post('/:cartid/:name', async (req, res) => {
+  //Check whether the product is a valid product
+  const result = await checkProduct(req.params.name, req.body.quantity)
+  //If product is valid, add it to cart. If not, return the error message
+  if (result !== 'Product not found!') {
+    //If cart exist, update the existing cart. If not, create a new cart
+    if (req.params.cartid !== 'null') {
+      await updateQuantity(req.params.cartid, req.params.name, result, res)
+      //Create new cart
+    } else {
+      const newCartitem = { item: [result] }
+      try {
+        // Create a new cart model
+        const insertedCartitem = await CartModel.create(newCartitem)
+        res.status(201).send(insertedCartitem)
+        }
+      catch(err) {
         res.status(500).send({ error: err.message })
+      }}
+  } else {
+    res.status(404).send({ error: result })
   }
 })
 
 //Update products in cart
 router.patch('/:cartid/:name', async (req, res) => {
-  const cartObject = await CartModel.findOne({ _id: req.params.cartid  })
-  if (cartObject) {
-    const productObject = await ProductModel.findOne({ name: req.params.name }) 
-    const { quantity } = req.body
-
-    const cartItem = cartObject.item
-    const filteredCartitem = cartItem.filter(item => item.product !== req.params.name)
-
-    const newCartitem = { product: productObject.name, quantity, price: productObject.price }
-    filteredCartitem.push(newCartitem)
-    const result = {item: filteredCartitem}
-
-
-  try {
-    //Find and update the product in cart
-    const cartItem = await CartModel.findByIdAndUpdate(req.params.cartid, result, { new: true})
-    if (cartItem) {
-      res.send(cartItem)
-    } else {
+  const result = await checkProduct(req.params.name, req.body.quantity)
+  if (result !== 'Product not found!') {
+    const cartObject = await CartModel.findOne({ _id: req.params.cartid  })
+    if (cartObject) {
+      await updateQuantity(req.params.cartid, req.params.name, result, res)
+    }else {
       res.status(404).send({error:'Cart Item not found!'})
-  }}
-  catch(err) {
-    res.status(500).send({ error: err.message})
-  }}
+    }
+  } else {
+    res.status(404).send({ error: result })
+  }
 })
 
 //Delete the product
