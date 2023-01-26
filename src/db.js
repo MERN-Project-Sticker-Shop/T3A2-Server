6a32e108603ea79c3cfcddce45c69e18cae8fad9
Chengqunniu <chengqunniu@gmail.com>
2023-01-26 16:10:50 +1100

    Created a model for order

--- src/db.js
+++ src/db.js
@@ -1,18 +1,18 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
-//Read .env file
+// Read .env file
 dotenv.config()  
 
 mongoose.set('strictQuery', true)
 
-//Close db connection
+// Close db connection
 async function dbClose() {
   await mongoose.connection.close()
   console.log('Database disconnected')
 }
 
-//Connect to the mongo database
+// Connect to the mongo database
 try{
   const m = await mongoose.connect(process.env.ATLAS_DB_URL) // obtain the environment variable
   console.log(m.connection.readyState === 1 ? 'Mongoose connected' : 'Mongoose failed to connect')
@@ -21,14 +21,14 @@ catch(err) {
   console.log(err)
 }
 
-//Create a schema and model for product
+// Create a schema and model for product
 const productSchema = await mongoose.Schema({
   name: { type:String, required:true},
   price: { type:Number, required:true},
   description: { type:String, required:true}
 })
 
-//Create a shema and model for cart
+// Create a shema and model for cart
 const cartSchema = await mongoose.Schema(
   {item: [{product: { type:String, required:true },
   price: { type:Number, required:true },
@@ -36,7 +36,15 @@ const cartSchema = await mongoose.Schema(
   }]}
 )
 
+// Create a schema and model for order
+const orderSchema = await mongoose.Schema({
+  content: { type:mongoose.isObjectIdOrHexString, ref: 'Cart'},
+  subtotal: { type:Number, required:true }
+})
+
+
 const CartModel = mongoose.model('Cart', cartSchema)
 const ProductModel = mongoose.model('Product', productSchema)
+const OrderModel = mongoose.model('Order', orderSchema)
 
-export { ProductModel, CartModel, dbClose }
+export { ProductModel, CartModel, OrderModel, dbClose }
