82fa10e539c43108162ab5b920ec652d6286e38e
Chengqunniu <chengqunniu@gmail.com>
2023-01-26 16:48:45 +1100

    Created a route to view single order details

--- src/routes/order_routes.js
+++ src/routes/order_routes.js
@@ -3,9 +3,24 @@ import { OrderModel } from '../db.js'
 
 const router = express.Router()
 
-//Get all products
+// Get all orders
 router.get('', async (req, res) => {
   res.send(await OrderModel.find().populate('cart'))
 })
+
+// Get a single order
+router.get('/:orderid', async (req, res) => {
+  try {
+    const order = await OrderModel.findOne({_id: req.params.orderid}).populate('cart')
+    if (order) {
+      res.send(order)
+    } else {
+      res.status(404).send({ error: 'Order not found' })
+    }}
+  catch (err) {
+    res.status(500).send({ error: err.message })
+  }
+})
+
 
 export default router
