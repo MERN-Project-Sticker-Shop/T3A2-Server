5e1048d126e88a0925223bb1975327bdf3f09e23
Chengqunniu <chengqunniu@gmail.com>
2023-01-27 23:26:30 +1100

    Test to view all orders

--- src/app.test.js
+++ src/app.test.js
@@ -197,5 +197,72 @@ describe("App tests", () => {
       expect(res.headers['content-type']).toMatch(/json/i)
       expect(res.body).toEqual({"error": "Product not found!"})
     })
+
+    // Test to get all orders
+    describe("GET order lists", () => {
+      let res
+      // Define the route and http method
+      beforeEach(async () => {   
+        res = await request(app).get('/orders')
+        expect(res.statusCode).toBe(200)
+        expect(res.headers['content-type']).toMatch(/json/i)
+        })
+      // Test the number of elements in the array
+      it('Should return an array of 2 elements', () => {        
+        expect(res.body).toBeInstanceOf(Array)
+        expect(res.body.length).toBe(2)
+      })
+      // Test the data structure of the data returned
+      it('Has an element with the correct data structure', () => {
+        res.body.forEach(el => {
+          expect(el._id).toBeDefined()
+          expect(el._id.length).toBe(24)
+          expect(el.total).toBeDefined()
+          expect(el.address).toBeDefined()
+          expect(el.cart).toBeDefined()
+          expect(el.cart.item).toBeDefined()
+        }) 
+      })
+      
+      // Test the value of the data returned
+      it('Has an element with the correct data value', () => {
+        expect(res.body[0].cart.item[0].product).toBe("63d35c8f2c13144a29ec8697") 
+        expect(res.body[0].cart.item[0].price).toBe(10)
+        expect(res.body[0].cart.item[0].quantity).toBe(1)
+        expect(res.body[0].cart.item[1].product).toBe("63d35c8f2c13144a29ec8698")  
+        expect(res.body[0].cart.item[1].price).toBe(15)
+        expect(res.body[0].cart.item[1].quantity).toBe(10)
+        expect(res.body[0].total).toBe(160)
+        expect(res.body[0].address.email).toBe("12345@gmail.com")
+        expect(res.body[0].cart.item[0].product).toBe("63d35c8f2c13144a29ec8698") 
+        expect(res.body[0].cart.item[0].price).toBe(15)
+        expect(res.body[0].cart.item[0].quantity).toBe(20)})
+    }) 
+
+    // Test to get a single order
+    describe("GET a single product", () => {
+      let res
+      // Define the route and http method
+      beforeEach(async () => {
+        res = await request(app).get('/orders/63d35c902c13144a29ec86a3')
+        expect(res.statusCode).toBe(200)
+        expect(res.headers['content-type']).toMatch(/json/i)      
+      })
+      // Test the value of the data returned
+      it('Has an element with the correct data value', () => {
+        expect(res.body.name).toBe("R U OK") 
+        expect(res.body.price).toBe(10)
+        expect(res.body.description).toBe("This is a sticker flakes")
+      })
+      // Test the data structure of the data returned
+      it('Has an element with the correct data structure', () => {
+          expect(res.body._id).toBeDefined()
+          expect(res.body.name).toBeDefined()
+          expect(res.body.price).toBeDefined()
+          expect(res.body.imageLinks).toBeDefined()
+          expect(res.body.description).toBeDefined()
+          expect(res.body._id.length).toBe(24)
+      })
+    })
 })
 
