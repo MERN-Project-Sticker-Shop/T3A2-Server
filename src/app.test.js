042b72d4ecf82ab1235811a5cd2ea8b6ae12cc8e
Chengqunniu <chengqunniu@gmail.com>
2023-01-28 12:50:42 +1100

    Test to view a single cart

--- src/app.test.js
+++ src/app.test.js
@@ -11,7 +11,6 @@ describe("App tests", () => {
     expect(res.headers['content-type']).toBe('application/json; charset=utf-8')
   })
 
-
   // Test the product routes to view all products
   describe("GET product lists", () => {
     let res
@@ -21,11 +20,13 @@ describe("App tests", () => {
       expect(res.statusCode).toBe(200)
       expect(res.headers['content-type']).toMatch(/json/i)
       })
+
     // Test the number of elements in the array
     it('Should return an array of 2 elements', () => {        
       expect(res.body).toBeInstanceOf(Array)
       expect(res.body.length).toBe(2)
     })
+
     // Test the data structure of the data returned
     it('Has an element with the correct data structure', () => {
       res.body.forEach(el => {
@@ -38,12 +39,13 @@ describe("App tests", () => {
 
       })
     })
+
     // Test the value of the data returned
     it('Has an element with the correct data value', () => {
       expect(res.body).toEqual(
       [
         {
-            "_id": "63d35c8f2c13144a29ec8697",
+            "_id": "63d479f8b4c1cb86f6d30b88",
             "name": "R U OK",
             "price": 10,
             "description": "This is a sticker flakes",
@@ -55,7 +57,7 @@ describe("App tests", () => {
             "__v": 0
         },
         {
-            "_id": "63d35c8f2c13144a29ec8698",
+            "_id": "63d479f8b4c1cb86f6d30b89",
             "name": "Autumn Vibes",
             "price": 15,
             "description": "This is a autumn sticker sheet",
@@ -68,22 +70,27 @@ describe("App tests", () => {
         }
       ])
     })
-  }) 
+  })
+
+
   // Test to get a single product
   describe("GET a single product with valid product name", () => {
     let res
+
     // Define the route and http method
     beforeEach(async () => {
       res = await request(app).get('/products/R U OK')
       expect(res.statusCode).toBe(200)
       expect(res.headers['content-type']).toMatch(/json/i)      
     })
+
     // Test the value of the data returned
     it('Has an element with the correct data value', () => {
       expect(res.body.name).toBe("R U OK") 
       expect(res.body.price).toBe(10)
       expect(res.body.description).toBe("This is a sticker flakes")
     })
+
     // Test the data structure of the data returned
     it('Has an element with the correct data structure', () => {
         expect(res.body._id).toBeDefined()
@@ -94,6 +101,7 @@ describe("App tests", () => {
         expect(res.body._id.length).toBe(24)
     })
 
+    // Test to view a single product with invalid name
     test('GET a single product with invalid name', async () => {
       const res = await request(app).get('/products/R U OKA')
       expect(res.statusCode).toBe(404)
@@ -101,6 +109,7 @@ describe("App tests", () => {
       expect(res.body).toEqual({"error": "Product not found!"})
     })
   })
+
     // Test the cart routes to get all carts
     describe("GET cart lists", () => {
       let res
@@ -111,11 +120,13 @@ describe("App tests", () => {
         expect(res.statusCode).toBe(200)
         expect(res.headers['content-type']).toMatch(/json/i)
         })
+
       // Test the number of elements in the array
       it('Should return an array of 2 elements', () => {        
         expect(res.body).toBeInstanceOf(Array)
         expect(res.body.length).toBe(2)
       })
+
       // Test the data structure of the data returned  
       it('Has an element with the correct data structure', () => {
         res.body.forEach(el => {
@@ -129,15 +140,16 @@ describe("App tests", () => {
           })  
         })
       })
+
       // Test the value of the data returned
       it('Has an element with the correct data value', () => {
         expect(res.body[0]).toEqual(
-          {
-              "_id": "63d35c8f2c13144a29ec869a",
+            {
+              "_id": "63d479f9b4c1cb86f6d30b8b",
               "item": [
                   {
                       "product": {
-                          "_id": "63d35c8f2c13144a29ec8697",
+                          "_id": "63d479f8b4c1cb86f6d30b88",
                           "name": "R U OK",
                           "description": "This is a sticker flakes",
                           "imageLinks": [
@@ -148,11 +160,11 @@ describe("App tests", () => {
                       },
                       "price": 10,
                       "quantity": 1,
-                      "_id": "63d35c8f2c13144a29ec869b"
+                      "_id": "63d479f9b4c1cb86f6d30b8c"
                   },
                   {
                       "product": {
-                          "_id": "63d35c8f2c13144a29ec8698",
+                          "_id": "63d479f8b4c1cb86f6d30b89",
                           "name": "Autumn Vibes",
                           "description": "This is a autumn sticker sheet",
                           "imageLinks": [
@@ -162,19 +174,90 @@ describe("App tests", () => {
                           ]
                       },
                       "price": 15,
-                      "quantity": 580,
-                      "_id": "63d35c8f2c13144a29ec869c"
+                      "quantity": 10,
+                      "_id": "63d479f9b4c1cb86f6d30b8d"
                   }
               ],
               "__v": 0
           })
         expect(res.body[1]).toEqual(
           {
-              "_id": "63d35c8f2c13144a29ec869d",
+            "_id": "63d479f9b4c1cb86f6d30b8e",
+            "item": [
+                {
+                    "product": {
+                        "_id": "63d479f8b4c1cb86f6d30b89",
+                        "name": "Autumn Vibes",
+                        "description": "This is a autumn sticker sheet",
+                        "imageLinks": [
+                            "https://ibb.co/yNBcb0k",
+                            "https://ibb.co/3WS3g9G",
+                            "https://ibb.co/ymTVQ9m"
+                        ]
+                    },
+                    "price": 15,
+                    "quantity": 20,
+                    "_id": "63d479f9b4c1cb86f6d30b8f"
+                }
+            ],
+            "__v": 0
+          })
+      })
+    }) 
+
+    // Test the cart routes to get a single cart
+    describe("GET a single cart", () => {
+        let res
+  
+        // Define the route and http method
+        beforeEach(async () => {   
+          res = await request(app).get('/carts/63d479f9b4c1cb86f6d30b8b')
+          expect(res.statusCode).toBe(200)
+          expect(res.headers['content-type']).toMatch(/json/i)
+          })
+    
+        // Test the number of elements in the array
+        it('Should return an array of 1 elements', () => {        
+          expect(res.body).toBeInstanceOf(Object)
+          expect(res.body.length).toBe(1)
+        })
+  
+        // Test the data structure of the data returned  
+        it('Has an element with the correct data structure', () => {
+          res.body.forEach(el => {
+            expect(res.body._id).toBeDefined()
+            expect(res.body.item).toBeDefined()
+            expect(res.body.item.product).toBeDefined()
+            expect(res.body.item.price).toBeDefined()
+            expect(res.body.item.quantity).toBeDefined()
+            expect(res.body.item._id).toBeDefined()
+          })
+        })
+    
+        // Test the value of the data returned
+        it('Has an element with the correct data value', () => {
+          expect(res.body).toEqual(
+            {
+              "_id": "63d479f9b4c1cb86f6d30b8b",
               "item": [
                   {
                       "product": {
-                          "_id": "63d35c8f2c13144a29ec8698",
+                          "_id": "63d479f8b4c1cb86f6d30b88",
+                          "name": "R U OK",
+                          "description": "This is a sticker flakes",
+                          "imageLinks": [
+                              "https://ibb.co/7vdXTsW",
+                              "https://ibb.co/DWTW5tV",
+                              "https://ibb.co/XJpPqPT"
+                          ]
+                      },
+                      "price": 10,
+                      "quantity": 1,
+                      "_id": "63d479f9b4c1cb86f6d30b8c"
+                  },
+                  {
+                      "product": {
+                          "_id": "63d479f8b4c1cb86f6d30b89",
                           "name": "Autumn Vibes",
                           "description": "This is a autumn sticker sheet",
                           "imageLinks": [
@@ -184,16 +267,18 @@ describe("App tests", () => {
                           ]
                       },
                       "price": 15,
-                      "quantity": 35,
-                      "_id": "63d35c8f2c13144a29ec869e"
-                  }]
-        })
-
-      })
-    }) 
-
+                      "quantity": 10,
+                      "_id": "63d479f9b4c1cb86f6d30b8d"
+                  }
+              ],
+              "__v": 0
+            })
+      }) 
+    })
+    
+    // Test the route to add product to the cart with valid cartid
     test("Add product to the cart with valid cartid", async () => {
-      const res = await request(app).post('/carts/63d35c8f2c13144a29ec869a/Autumn Vibes').send({
+      const res = await request(app).post('/carts/63d479f9b4c1cb86f6d30b8b/Autumn Vibes').send({
         quantity: 15
       })
       expect(res.status).toBe(201)
@@ -209,6 +294,7 @@ describe("App tests", () => {
       expect(res.body.item[1].quantity).toBe(25)    
     })
 
+    // Test the route to add product to the cart with invalid cartid
     test("Add product to the cart with invalid cartid", async () => {
       const res = await request(app).post('/carts/63d35c8f2c13144a29ec899a/Autumn Vibes').send({
         quantity: 15
@@ -218,6 +304,7 @@ describe("App tests", () => {
       expect(res.body).toEqual({"error": "Cart Item not found!"})
     })
 
+    // Test the route to add product to the cart without cartid, will create a new cart
     test("Add product to the cart without cartid", async () => {
       const res = await request(app).post('/carts/null/R U OK').send({
         quantity: 15
@@ -231,8 +318,9 @@ describe("App tests", () => {
       expect(res.body.item[0].quantity).toBe(15)
     })
 
+    // Test the route to add product to the cart with invalid product name
     test("Add product to the cart with invalid product name", async () => {
-      const res = await request(app).post('/carts/null/R U OKA').send({
+      const res = await request(app).post('/carts/63d479f9b4c1cb86f6d30b8b/R U OKA').send({
         quantity: 15
       })
       expect(res.status).toBe(404)
@@ -240,8 +328,9 @@ describe("App tests", () => {
       expect(res.body).toEqual({"error": "Product not found!"})
     })
 
+    // Test the route to update product quantity in the cart with valid cartid
     test("Update product quantity to the cart with valid id", async () => {
-      const res = await request(app).post('/carts/63d35c8f2c13144a29ec869d/Autumn Vibes').send({
+      const res = await request(app).patch('/carts/63d479f9b4c1cb86f6d30b8e/Autumn Vibes').send({
         quantity: 10
       })
       expect(res.status).toBe(201)
@@ -250,11 +339,12 @@ describe("App tests", () => {
       expect(res.body.item[0].product.name).toBe("Autumn Vibes") 
       expect(res.body.item[0].product.description).toBe("This is a autumn sticker sheet")
       expect(res.body.item[0].price).toBe(15)
-      expect(res.body.item[0].quantity).toBe(25)
+      expect(res.body.item[0].quantity).toBe(30)
     })
 
+    // Test the route to update product quantity in the cart with invalid cartid
     test("Update product quantity to the cart with invalid cartid", async () => {
-      const res = await request(app).post('/carts/63d35c8f2d13144a29ec869d/Autumn Vibes').send({
+      const res = await request(app).patch('/carts/63d35c8f2d13144a29ec869d/Autumn Vibes').send({
         quantity: 10
       })
       expect(res.status).toBe(404)
@@ -262,8 +352,9 @@ describe("App tests", () => {
       expect(res.body).toEqual({"error": "Cart Item not found!"})
     })
 
+    // Test the route to update product quantity in the cart with invalid product name
     test("Update product quantity to the cart with invalid product name", async () => {
-      const res = await request(app).post('/carts/63d35c8f2c13144a29ec869d/Autumn and Spring Vibes').send({
+      const res = await request(app).patch('/carts/63d479f9b4c1cb86f6d30b8e/Autumn and Spring Vibes').send({
         quantity: 10
       })
       expect(res.status).toBe(404)
@@ -309,8 +400,9 @@ describe("App tests", () => {
         expect(res.body[0].address.email).toBe("12345@gmail.com")
         expect(res.body[0].cart.item[0].product).toBe("63d35c8f2c13144a29ec8698") 
         expect(res.body[0].cart.item[0].price).toBe(15)
-        expect(res.body[0].cart.item[0].quantity).toBe(20)})
-    }) 
+        expect(res.body[0].cart.item[0].quantity).toBe(20)
+      })
+    })
 
     // Test to get a single order
     describe("GET a single product with valid orderid", () => {
@@ -409,4 +501,3 @@ describe("App tests", () => {
       expect(res.body).toEqual({"error": "Cart not found!"})
     })
 })
-
